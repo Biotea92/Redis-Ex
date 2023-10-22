fc7634ab1a641d897766d2e55458c12546f1d5f1
Kim woo jong <skekdnwhd@gmail.com>
2023-10-22 23:29:25 +0900

    redis 캐싱 예제 프로젝트 생성

---
+++ RedisCasching/src/main/java/com/example/RedisCasching/RedisCaschingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedisCasching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCaschingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCaschingApplication.class, args);
+	}
+
+}
---
+++ RedisCasching/src/test/java/com/example/RedisCasching/RedisCaschingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RedisCasching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCaschingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
