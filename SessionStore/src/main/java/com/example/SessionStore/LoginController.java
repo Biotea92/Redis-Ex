52a24b3b48ca71f0bb2f882ad69befadf5e25ad8
Kim woo jong <skekdnwhd@gmail.com>
2023-10-22 22:21:56 +0900

    redis를 이용한 세션관리

--- SessionStore/src/main/java/com/example/SessionStore/LoginController.java
+++ SessionStore/src/main/java/com/example/SessionStore/LoginController.java
@@ -6,26 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
 
-    Map<String, String> sesstionMap = new ConcurrentHashMap<>();
-
     @GetMapping("/login")
     public String login(HttpSession session, @RequestParam String name) {
-        sesstionMap.put(session.getId(), name);
+        session.setAttribute("name", name);
 
         return "saved session";
     }
 
     @GetMapping("/myName")
     public String myName(HttpSession session) {
-        String myName = sesstionMap.get(session.getId());
-
+        String myName = (String) session.getAttribute("name");
         return myName;
     }
 }
